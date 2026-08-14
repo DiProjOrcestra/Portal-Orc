741b998e00763aaa91b8add924fc7bb7bd7ae148
Natan França <natan.j.franca@gmail.com>
2026-08-14 20:26:52 -0300

    fix: corrigir importação errada e falta de endpoint de registro

--- portal_orc/src/main/java/com/orcestra/portal_orc/controller/AuthenticationController.java
+++ portal_orc/src/main/java/com/orcestra/portal_orc/controller/AuthenticationController.java
@@ -3,15 +3,16 @@ package com.orcestra.portal_orc.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orcestra.portal_orc.dto.UserRequestDto;
 import com.orcestra.portal_orc.exception.BadRequestException;
-import com.service.AuthenticationService;
+import com.orcestra.portal_orc.service.AuthenticationService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -23,7 +24,7 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping()
+    @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public void register(@Valid @RequestBody UserRequestDto userRequestDto) throws BadRequestException{
         authenticationService.registerUser(userRequestDto);
