057ff546994d86934f16ad4db7b6b2ab10f9253b
Natan França <natan.j.franca@gmail.com>
2026-08-14 20:32:58 -0300

    feat: adicionar exceção de "não encontrado"

--- portal_orc/src/main/java/com/orcestra/portal_orc/handler/GlobalExceptionHandler.java
+++ portal_orc/src/main/java/com/orcestra/portal_orc/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.orcestra.portal_orc.handler;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.orcestra.portal_orc.exception.BadRequestException;
 import com.orcestra.portal_orc.exception.ErrorResponse;
+import com.orcestra.portal_orc.exception.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +21,15 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
 
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException notFoundException) {
+        
+        ErrorResponse errorResponse = ErrorResponse.builder().message(notFoundException.getMessage()).status(HttpStatus.NOT_FOUND.value()).build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
     }
 }
