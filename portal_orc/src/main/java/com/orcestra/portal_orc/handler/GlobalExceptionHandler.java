1b69b887192f06d586cea7f190f9d032229c2c9e
Natan8643 <natan.j.franca@gmail.com>
2026-08-20 19:19:55 -0300

    feat: adicionar manipulador de exceções da dependência Validation

--- portal_orc/src/main/java/com/orcestra/portal_orc/handler/GlobalExceptionHandler.java
+++ portal_orc/src/main/java/com/orcestra/portal_orc/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.orcestra.portal_orc.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -30,6 +31,16 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException (MethodArgumentNotValidException methodArgumentNotValidException) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder().message(methodArgumentNotValidException.getMessage()).status(HttpStatus.BAD_REQUEST.value()).build();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
     }
 }
