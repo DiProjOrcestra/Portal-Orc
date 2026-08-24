e6605bf5d1303b01d5e3476f37160fddb2c28dcc
Thiago Oliveira <thiagooliveira1403@gmail.com>
2026-08-24 10:17:46 -0300

    chore: apaga pastas duplicadas

--- portal_orc/src/main/java/com/orcestra/portal_orc/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.orcestra.portal_orc.exception;
-
-public class BadRequestException extends Exception {
-    public BadRequestException(String message){
-        super(message);
-    }
-}
--- portal_orc/src/main/java/com/orcestra/portal_orc/repository/DirectorateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orcestra.portal_orc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.orcestra.portal_orc.model.DirectorateEntity;
-
-public interface DirectorateRepository extends JpaRepository <DirectorateEntity, Integer>{
-    
-}
