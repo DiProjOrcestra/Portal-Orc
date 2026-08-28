5fc98cce1f89e0e84d63d315122631f5561b44f8
Natan França <natan.j.franca@gmail.com>
2026-08-27 22:45:46 -0300

    feat: adiciona assincronidade a fução de enviar email

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/PortalOrcApplication.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/PortalOrcApplication.java
@@ -2,8 +2,10 @@ package com.orcestra.portal_orc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PortalOrcApplication {
 
 	public static void main(String[] args) {
--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/EmailSenderService.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/EmailSenderService.java
@@ -3,6 +3,7 @@ package com.orcestra.portal_orc.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,8 @@ public class EmailSenderService {
     @Value("${spring.mail.username}")
     private String senderEmail;
     private final JavaMailSender mailSender;
-
+    
+    @Async
     public void sendEmail(String receiverEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(senderEmail);
