b68a19ecf17552907605e2929cfccc4fb67582cc
Natan8643 <natan.j.franca@gmail.com>
2026-09-02 20:07:29 -0300

    fix: altera porta do cors para acessar o vite

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/config/SecurityConfiguration.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); 
+        config.setAllowedOrigins(List.of("http://localhost:5173")); 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
