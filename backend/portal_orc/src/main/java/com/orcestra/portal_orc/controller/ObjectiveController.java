cbee31e31c45c475b9e3ddd347895924da491441
Natan França <natan.j.franca@gmail.com>
2026-09-08 20:43:23 -0300

    feat: adiciona funçao de editar objetivo

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/controller/ObjectiveController.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/controller/ObjectiveController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.orcestra.portal_orc.dto.ObjectiveRequestDto;
 import com.orcestra.portal_orc.dto.ObjectiveResponseDto;
+import com.orcestra.portal_orc.exception.NotFoundException;
 import com.orcestra.portal_orc.service.ObjectiveService;
 
 import jakarta.validation.Valid;
@@ -38,4 +41,9 @@ public class ObjectiveController {
         return objectiveService.getAllObjective();
     }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ObjectiveResponseDto updateObjective(@PathVariable Integer id, @RequestBody ObjectiveRequestDto objectiveRequestDto) throws NotFoundException {
+        return objectiveService.updateObjective(id, objectiveRequestDto);
+    }
 }
