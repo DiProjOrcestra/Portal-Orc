7f5b93578684d40b77a29266ad24345bc94ad055
Natan França <natan.j.franca@gmail.com>
2026-09-08 20:43:23 -0300

    feat: adiciona funçao de editar objetivo

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/ObjectiveService.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/ObjectiveService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.orcestra.portal_orc.dto.ObjectiveRequestDto;
 import com.orcestra.portal_orc.dto.ObjectiveResponseDto;
+import com.orcestra.portal_orc.exception.NotFoundException;
 import com.orcestra.portal_orc.model.ObjectiveEntity;
 import com.orcestra.portal_orc.repository.ObjectiveRepository;
 
@@ -25,5 +26,15 @@ public class ObjectiveService {
         return objectiveRepository.findAll().stream()
                                             .map(ObjectiveResponseDto::new )
                                             .toList();
-    } 
+    }
+    
+    public ObjectiveResponseDto updateObjective(Integer id, ObjectiveRequestDto objectiveRequestDto) throws NotFoundException {
+        ObjectiveEntity objective = objectiveRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Objetivo com id %d não existe", id)));
+        
+        if(objectiveRequestDto.getDescription() != null) objective.setDescription(objectiveRequestDto.getDescription());
+
+        objectiveRepository.save(objective);
+
+        return new ObjectiveResponseDto(objective);
+    }
 }
