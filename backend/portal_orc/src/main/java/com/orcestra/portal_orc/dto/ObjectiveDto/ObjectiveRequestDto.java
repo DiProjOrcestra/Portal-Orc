99a5ad32c2becdadf50567904061be9f991468d2
Natan8643 <natan.j.franca@gmail.com>
2026-09-10 09:25:19 -0300

    feat: adiciona validação de campos na requisição

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/dto/ObjectiveDto/ObjectiveRequestDto.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/dto/ObjectiveDto/ObjectiveRequestDto.java
@@ -1,6 +1,7 @@
 package com.orcestra.portal_orc.dto.ObjectiveDto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Setter 
 public class ObjectiveRequestDto {
     
+    @Schema(example = "Atingir CSAT com cliente > 4.5")
     @NotBlank(message = "O campo descrição não pode ser vazio")
     private String description;
 }
