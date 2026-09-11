644576e1b730675880a54811ee9c4b8ee0a0240e
Natan França <natan.j.franca@gmail.com>
2026-09-11 00:28:08 -0300

    feat: adiciona função de editar e excluir plano de ação

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/controller/ActionPlanController.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/controller/ActionPlanController.java
@@ -3,7 +3,9 @@ package com.orcestra.portal_orc.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +14,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orcestra.portal_orc.dto.LinkUsersToActionPlanRequestDto;
+import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanRequestDto;
 import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanResponseDto;
+import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanStatusRequestDto;
+import com.orcestra.portal_orc.exception.BadRequestException;
 import com.orcestra.portal_orc.exception.NotFoundException;
 import com.orcestra.portal_orc.service.ActionPlanService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor 
@@ -31,8 +37,33 @@ public class ActionPlanController {
         return actionPlanService.getAllActionPlan();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}/link-user")
+    @ResponseStatus(HttpStatus.OK)
     public void linkUserToActionPlan(@PathVariable Integer id, @RequestBody LinkUsersToActionPlanRequestDto requestDto) throws NotFoundException {
         actionPlanService.linkUserToActionPlan(id, requestDto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateActionPlan(@PathVariable Integer id, @Valid @RequestBody ActionPlanRequestDto actionPlanRequestDto) throws NotFoundException, BadRequestException {
+        actionPlanService.updateActionPlan(id, actionPlanRequestDto);
+    }
+
+    @PatchMapping("/{id}/status")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateActionPlanStatus(@PathVariable Integer id, @Valid @RequestBody ActionPlanStatusRequestDto actionPlanStatusRequestDto) throws NotFoundException {
+        actionPlanService.updateActionPlanStatus(id, actionPlanStatusRequestDto);
+    }
+
+    @PatchMapping("/{actionPlanId}/subtask/{subtaskId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateActionPlanSubtask(@PathVariable Integer actionPlanId, @PathVariable Integer subtaskId) throws NotFoundException {
+        actionPlanService.updateActionPlanSubtask(actionPlanId, subtaskId);
+    }
+
+    @DeleteMapping("/{id}") 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteActionPlan(@PathVariable Integer id) throws NotFoundException {
+        actionPlanService.deleteActionPlan(id);
     }
 }
--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/ActionPlanService.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/ActionPlanService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.orcestra.portal_orc.dto.LinkUsersToActionPlanRequestDto;
 import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanRequestDto;
 import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanResponseDto;
+import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanStatusRequestDto;
 import com.orcestra.portal_orc.exception.BadRequestException;
 import com.orcestra.portal_orc.exception.NotFoundException;
 import com.orcestra.portal_orc.model.ActionPlanEntity;
@@ -19,6 +20,7 @@ import com.orcestra.portal_orc.model.UserEntity;
 import com.orcestra.portal_orc.repository.ActionPlanRepository;
 import com.orcestra.portal_orc.repository.DirectorateRepository;
 import com.orcestra.portal_orc.repository.ObjectiveRepository;
+import com.orcestra.portal_orc.repository.SubtaskRepository;
 import com.orcestra.portal_orc.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -31,6 +33,7 @@ public class ActionPlanService {
     private final DirectorateRepository directorateRepository;
     private final ObjectiveRepository objectiveRepository;
     private final UserRepository userRepository;
+    private final SubtaskRepository subtaskRepository;
 
     public void createActionPlan(Integer objectiveId, ActionPlanRequestDto actionPlanRequestDto) throws BadRequestException, NotFoundException {
 
@@ -95,5 +98,68 @@ public class ActionPlanService {
         }
         actionPlan.getUsers().addAll(users);
         actionPlanRepository.save(actionPlan);                                                                
+    }
+
+    public void updateActionPlan(Integer actionPlanId, ActionPlanRequestDto actionPlanRequestDto) throws NotFoundException, BadRequestException {
+
+        ActionPlanEntity actionPlan = actionPlanRepository.findById(actionPlanId).orElseThrow(() -> 
+                                            new NotFoundException(String.format("Plano de ação com id %d não existe", actionPlanId)));
+        actionPlan.setName(actionPlanRequestDto.getName());
+        actionPlan.setTerm(actionPlanRequestDto.getTerm());
+        actionPlan.setProgress(actionPlanRequestDto.getProgress());
+        actionPlan.setDirectorate(directorateRepository.findByDirectorateName(
+                                                        actionPlanRequestDto
+                                                                            .getDirectorate()
+                                                                            .name())
+                                                                            .orElseThrow(() -> new 
+                                                                                                BadRequestException(String
+                                                                                                .format("Diretoria com nome %s não existe", 
+                                                                                                            actionPlanRequestDto.getDirectorate()
+                                                                                                                                .name()))));
+        actionPlan.setPriority(actionPlanRequestDto.getPriority());
+        List<SubtaskEntity> subtasks = actionPlanRequestDto.getSubtasks()
+                                                            .stream()
+                                                            .map(SubtaskEntity::new)
+                                                            .toList();
+
+        subtasks.forEach(subtask -> subtask.setActionPlan(actionPlan));
+        actionPlan.setSubtasks(subtasks);
+        Set<UserEntity> users = new HashSet<>();
+
+        for (String id : actionPlanRequestDto.getUsersId()) {
+            String cpf = id.replaceAll("\\D", "");
+            UserEntity user = userRepository.findById(cpf)
+                                            .orElseThrow(() -> new NotFoundException(
+                                                                    String.format("Usuário com CPF %s não existe",cpf)));
+            users.add(user);
+        }
+        actionPlan.setUsers(users);
+        actionPlanRepository.save(actionPlan); 
+    }
+
+    public void updateActionPlanStatus(Integer id, ActionPlanStatusRequestDto actionPlanStatusRequestDto) throws NotFoundException {
+        ActionPlanEntity actionPlan = actionPlanRepository.findById(id)
+                                                            .orElseThrow(() -> new 
+                                                                            NotFoundException(String.format("Plano de ação com id %d não existe", id)));
+        if(actionPlanStatusRequestDto.getProgress() != null) actionPlan.setProgress(actionPlanStatusRequestDto.getProgress());
+        actionPlanRepository.save(actionPlan);
+    }
+
+    public void updateActionPlanSubtask(Integer actionPlanId, Integer subtaskId) throws NotFoundException {
+        actionPlanRepository.findById(actionPlanId).orElseThrow(
+                                                     () -> new NotFoundException(String.format("Plano de ação com id %d não existe", actionPlanId)));
+        
+        SubtaskEntity subtask = subtaskRepository.findById(subtaskId).orElseThrow(
+                                                     () -> new NotFoundException(String.format("Tarefa com id %d não existe", subtaskId)));
+
+        subtask.setDone(!subtask.getDone());
+        subtaskRepository.save(subtask);
+    }
+
+    public void deleteActionPlan(Integer actionPlanId) throws NotFoundException {
+        ActionPlanEntity actionPlan = actionPlanRepository.findById(actionPlanId).orElseThrow(
+                                                     () -> new NotFoundException(String.format("Plano de ação com id %d não existe", actionPlanId)));
+
+        actionPlanRepository.delete(actionPlan);
     }
 }
