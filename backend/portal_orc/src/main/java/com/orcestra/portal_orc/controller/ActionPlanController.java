b0b482386ee121cdf9f0c10bdeae76e227f79ae3
Natan França <natan.j.franca@gmail.com>
2026-09-10 21:37:32 -0300

    feat: adiciona função de vincular membros ao plano de ação

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/controller/ActionPlanController.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/controller/ActionPlanController.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orcestra.portal_orc.dto.LinkUsersToActionPlanRequestDto;
 import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanResponseDto;
+import com.orcestra.portal_orc.exception.NotFoundException;
 import com.orcestra.portal_orc.service.ActionPlanService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,5 +29,10 @@ public class ActionPlanController {
     @ResponseStatus(HttpStatus.OK)
     public List<ActionPlanResponseDto> getActionPlan() {
         return actionPlanService.getAllActionPlan();
+    }
+
+    @PutMapping("/{id}")
+    public void linkUserToActionPlan(@PathVariable Integer id, @RequestBody LinkUsersToActionPlanRequestDto requestDto) throws NotFoundException {
+        actionPlanService.linkUserToActionPlan(id, requestDto);
     }
 }
--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/ActionPlanService.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/service/ActionPlanService.java
@@ -1,9 +1,12 @@
 package com.orcestra.portal_orc.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.orcestra.portal_orc.dto.LinkUsersToActionPlanRequestDto;
 import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanRequestDto;
 import com.orcestra.portal_orc.dto.ActionPlanDto.ActionPlanResponseDto;
 import com.orcestra.portal_orc.exception.BadRequestException;
@@ -12,9 +15,11 @@ import com.orcestra.portal_orc.model.ActionPlanEntity;
 import com.orcestra.portal_orc.model.DirectorateEntity;
 import com.orcestra.portal_orc.model.ObjectiveEntity;
 import com.orcestra.portal_orc.model.SubtaskEntity;
+import com.orcestra.portal_orc.model.UserEntity;
 import com.orcestra.portal_orc.repository.ActionPlanRepository;
 import com.orcestra.portal_orc.repository.DirectorateRepository;
 import com.orcestra.portal_orc.repository.ObjectiveRepository;
+import com.orcestra.portal_orc.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +30,7 @@ public class ActionPlanService {
     private final ActionPlanRepository actionPlanRepository;
     private final DirectorateRepository directorateRepository;
     private final ObjectiveRepository objectiveRepository;
+    private final UserRepository userRepository;
 
     public void createActionPlan(Integer objectiveId, ActionPlanRequestDto actionPlanRequestDto) throws BadRequestException, NotFoundException {
 
@@ -47,7 +53,19 @@ public class ActionPlanService {
                                                                 .stream()
                                                                 .map(SubtaskEntity::new)
                                                                 .toList(); 
-                                                                
+        
+        Set<UserEntity> users = new HashSet<>();
+                                                
+        for (String id : actionPlanRequestDto.getUsersId()) {
+            String cpf = id.replaceAll("\\D", "");
+            UserEntity user = userRepository.findById(cpf).orElse(null);
+            if (user == null) {
+                throw new NotFoundException(String.format("Usuário com CPF %s não existe", cpf));
+            }
+            users.add(user);
+        }
+
+        actionPlan.setUsers(users);
         actionPlan.setObjective(objective);
         actionPlan.setSubtasks(subtasks);
         actionPlan.setDirectorate(directorate);
@@ -59,4 +77,23 @@ public class ActionPlanService {
     public List<ActionPlanResponseDto> getAllActionPlan() {
         return actionPlanRepository.findAll().stream().map(ActionPlanResponseDto::new ).toList();        
     }
-}   
+
+    public void linkUserToActionPlan(Integer actionPlanId, LinkUsersToActionPlanRequestDto requestDto) throws NotFoundException {
+        
+        ActionPlanEntity actionPlan = actionPlanRepository.findById(actionPlanId).orElseThrow(() -> new 
+                                                NotFoundException(String.format("Plano de ação com id %d não existe", actionPlanId)));
+
+        Set<UserEntity> users = new HashSet<>();
+                                                
+        for (String id : requestDto.getUsersId()) {
+            String cpf = id.replaceAll("\\D", "");
+            UserEntity user = userRepository.findById(cpf).orElse(null);
+            if (user == null) {
+                throw new NotFoundException(String.format("Usuário com CPF %s não existe", cpf));
+            }
+            users.add(user);
+        }
+        actionPlan.getUsers().addAll(users);
+        actionPlanRepository.save(actionPlan);                                                                
+    }
+}
