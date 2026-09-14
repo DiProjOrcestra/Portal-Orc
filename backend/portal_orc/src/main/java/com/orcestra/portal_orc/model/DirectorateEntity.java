6a54a8ae7e9f00ef5f7ca26794eb730026776a2c
Natan França <natan.j.franca@gmail.com>
2026-09-14 13:47:13 -0300

    fix: adiciona autoincremeto de id na diretoria

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/model/DirectorateEntity.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/model/DirectorateEntity.java
@@ -3,8 +3,11 @@ package com.orcestra.portal_orc.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -24,6 +27,7 @@ import lombok.Setter;
 public class DirectorateEntity {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String directorateName;
