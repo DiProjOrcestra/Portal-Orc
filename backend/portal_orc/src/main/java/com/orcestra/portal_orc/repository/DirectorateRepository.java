bcfca7de37924893075872f757c55956b300e1ac
Natan França <natan.j.franca@gmail.com>
2026-09-14 12:18:27 -0300

    fix: adiciona arquivos faltantes devido ao cherry pick

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/repository/DirectorateRepository.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/repository/DirectorateRepository.java
@@ -3,7 +3,10 @@ package com.orcestra.portal_orc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.orcestra.portal_orc.model.DirectorateEntity;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface DirectorateRepository extends JpaRepository <DirectorateEntity, Integer>{
-    
+    Optional<DirectorateEntity> findByDirectorateName(String directorateName);
 }
