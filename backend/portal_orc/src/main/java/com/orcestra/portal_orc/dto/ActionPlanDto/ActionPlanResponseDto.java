31cc9585e260ae1a669af458cc8905d084d243cb
Natan8643 <natan.j.franca@gmail.com>
2026-09-16 12:59:04 -0300

    refactor: retira import nao utilizado

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/dto/ActionPlanDto/ActionPlanResponseDto.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/dto/ActionPlanDto/ActionPlanResponseDto.java
@@ -2,7 +2,6 @@ package com.orcestra.portal_orc.dto.ActionPlanDto;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
