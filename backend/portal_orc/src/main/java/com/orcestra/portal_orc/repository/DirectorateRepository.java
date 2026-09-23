88d3d5b0fd0d28739a636e919181a3a603ca61c9
Natan França <natan.j.franca@gmail.com>
2026-09-23 19:54:59 -0300

    refactor: remove import inutilizado

--- backend/portal_orc/src/main/java/com/orcestra/portal_orc/repository/DirectorateRepository.java
+++ backend/portal_orc/src/main/java/com/orcestra/portal_orc/repository/DirectorateRepository.java
@@ -2,8 +2,6 @@ package com.orcestra.portal_orc.repository;
 
 import java.util.Optional;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.orcestra.portal_orc.model.DirectorateEntity;
